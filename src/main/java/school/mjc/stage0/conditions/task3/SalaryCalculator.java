7946326377531c2a0d58d23919b568f74ac47b1e
Batuhan Derya <bderya@3-G-9.42heilbronn.de>
2022-09-24 20:51:48 +0200

    FirstTest

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,21 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-		if (salary <= 10000 )
+		if (salary < 0) 
 		{
-			System.out.println((salary/100) * 85);
-		}
-		else if (salary <= 20000 )
+            System.out.println("wrong input!");
+        } 
+		else if (salary <= 10000)
 		{
-			System.out.println((salary/100) * 82);
-		}
-		else if (salary > 20000 )
+            System.out.println(salary * 0.85);
+        } 
+		else if (salary < 20001)
 		{
-			System.out.println((salary/100) * 80);
-		}
+            System.out.println(salary * 0.82);
+        } 
 		else
 		{
-			System.out.println("wrong input!");
-		}
+            System.out.println(salary * 0.8);
+        }
     }
 }
